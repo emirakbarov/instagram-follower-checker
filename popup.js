b09716b9b28c8b6a52e77f2ada531d9740fe5bda
emirakbarov <126677803+emirakbarov@users.noreply.github.com>
2025-06-23 22:14:53 +0100

    add instructions fix closebtn error

--- popup.js
+++ popup.js
@@ -1,3 +1,16 @@
 document.getElementById("username-form").addEventListener("submit", () => {
   document.getElementById("loadingSpinner").style.display = "block";
 });
+
+document.getElementById("howToLink").addEventListener("click", () => {
+  chrome.windows.create({
+    url: chrome.runtime.getURL("howto.html"),
+    type: "popup",
+    width: 420,
+    height: 500
+  });
+});
+
+document.getElementById("closeBtn").addEventListener("click", () => {
+    window.close();
+});
